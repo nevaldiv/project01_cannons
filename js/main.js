421ea79c0108c14ac51838697484f5f5714b27bd
nelson valdivia <nperez88@gmail.com>
2016-01-31 16:50:53 -0800

    adds prompt asking player names

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
+var p1Name;
+var p2Name;
+var p1Model;
+var p2Model;
+var particle;
+var terrain;
 
 
 $(function() {
 
-console.log("main.js loaded!");
+//BASICS. function dictates start screen to game.
+function getNames(p1Name, p2Name) {
+  var p1Name = prompt("Good Hello", "please enter p1 name");
+  var p2Name = prompt("Good Hello", "please enter p1 name");
+  console.log(p1Name);
+  console.log(p2Name);
+}
+getNames();
+
+
+
+
+
+
+
 
 
 
