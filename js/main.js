b6fdf51a96fc30bbefa7e3ae7ffad374b895cbd2
nelson valdivia <nperez88@gmail.com>
2016-02-04 16:59:01 -0800

    added tank drawings, hitboxes, win, and turn conditions and functions

--- js/main.js
+++ js/main.js
@@ -1,60 +1,6 @@
-// var p1Name;
-// var p2Name;
-// var angle;
-// var p1Model;
-// var p2Model;
-// var particle;
-// var terrain;
-
-// //BASICS. function dictates start screen to game.
-// function getNames(p1Name, p2Name) {
-//   var p1Name = prompt("Good Hello", "please enter p1 name");
-//   var p2Name = prompt("Good Hello", "please enter p1 name");
-//   console.log(p1Name);
-//   console.log(p2Name);
-// }
-// getNames();
-
-// //MVP of getting and parsing into numbers angle and velocity
-// function getAngle(playerAngle, angle) {
-//   var playerAngle = prompt("Angle:", "Please enter the Angle to shoot");
-//   //parsing string into num, unsure if nessessary yet.
-//   var angle = parseInt(playerAngle);
-//   console.log(angle);
-// }
-// getAngle();
-
-// //MVP of getting and parsing into numbers angle and velocity
-// function getAngle(playerAngle, angle) {
-//   var playerAngle = prompt("Angle:", "Please enter the Angle to shoot");
-//   //parsing string into num, unsure if nessessary yet.
-//   var angle = parseInt(playerAngle);
-//   console.log(angle);
-// }
-// getAngle();
-
-// var Projectile = function(xpos,ypos,key3,key4) {
-//   this.xloc = xpos;
-//   this.yloc = ypos;
-//   this.data = key3;
-//   this.data2= key4;
-// }
-
-// NOW we bring it all together
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var bullet;
+var ticker = 0;
+var victory;
 
 var canvas = document.getElementById('myCanvas');
 var angleInput = document.getElementById('angle');
@@ -62,7 +8,8 @@ var velocityInput = document.getElementById('velocity');
 var launchButton = document.getElementById('launchButton');
 var ctx = canvas.getContext("2d");
 
-
+//example structure for a shot object
+//included at top for testing fireCanón(shot);
 var shot = {
   xInitial: 100,
   yInitial: 300,
@@ -97,8 +44,95 @@ function drawCanvas() {
   // draw land
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 400, canvas.width, 50);
+
+  drawP1Tank();
+  drawP2Tank();
 }
 
+function drawP1Tank() {
+
+  //save ctx settings since therell be ctx rotation later
+  ctx.save();
+
+  ctx.translate(50,364);
+  ctx.strokeStyle = "#888";
+  ctx.lineWidth = 2;
+
+  ctx.strokeRect(5, 20, 30, 5); // body
+  ctx.strokeRect(15, 15, 10, 5); //head
+
+  //treads
+  ctx.beginPath();
+  ctx.lineTo(5, 35);
+  ctx.lineTo(35, 35);
+  ctx.stroke();
+  ctx.beginPath();
+
+  //wheels
+  ctx.arc(5, 30, 5, 0, Math.PI*2, false);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(15, 30, 5, 0, Math.PI*2, false);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(25, 30, 5, 0, Math.PI*2, false);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(35, 30, 5, 0, Math.PI*2, false);
+  ctx.stroke();
+  //barell
+  ctx.translate(27,18);
+  ctx.rotate(315 * Math.PI / 180);
+  ctx.strokeRect(0, 0, 17, 2);
+  //restores the rotation to 0
+  ctx.restore();
+
+}
+function drawP2Tank() {
+
+
+  ctx.save();
+  // translate context to center of canvas
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+
+  // flip context vertically
+  ctx.scale(-1, 1);
+  //save ctx settings since therell be ctx rotation later
+
+  ctx.translate(-300,139);
+  ctx.strokeStyle = "#888";
+  ctx.lineWidth = 2;
+
+  ctx.strokeRect(5, 20, 30, 5); // body
+  ctx.strokeRect(15, 15, 10, 5); //head
+
+  //treads
+  ctx.beginPath();
+  ctx.lineTo(5, 35);
+  ctx.lineTo(35, 35);
+  ctx.stroke();
+  ctx.beginPath();
+
+  //wheels
+  ctx.arc(5, 30, 5, 0, Math.PI*2, false);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(15, 30, 5, 0, Math.PI*2, false);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(25, 30, 5, 0, Math.PI*2, false);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(35, 30, 5, 0, Math.PI*2, false);
+  ctx.stroke();
+  //barell
+  ctx.translate(27,18);
+  ctx.rotate(315 * Math.PI / 180);
+  ctx.strokeRect(0, 0, 17, 2);
+  //restores the rotation to 0
+  ctx.restore();
+
+}
 
 function drawCircle(coordinates) {
   ctx.beginPath();
@@ -110,28 +144,32 @@ function drawCircle(coordinates) {
   ctx.fill();
 }
 
-var time = 0;
 
+
+var time = 0;
 function renderShot() {
   drawCanvas();
   drawCircle(positionAtTime(time));
   console.log("Look out below!!!");
-
   time += 10;
-  if (positionAtTime(time)[1] < 410) {
-    setTimeout(renderShot, 10);
-  } else {
-    launchButton1.disabled = false;
-    launchButton2.disabled = false;
 
+
+  if(checkPlayerHitByBullet(time) === true) {
+        console.log("TEST");
+
+  } else {
+    if (positionAtTime(time)[1] < 410) {
+      setTimeout(renderShot, 10); //loop by calling self every 10ms if Pow above horizon
+    } else {
+      ticker +=1;
+      whosTurnIsIt(ticker);
+    }
   }
 }
 
 
-
-
-
-
+//phil put this function in, I opted for contructor
+//function "Pow" instead
 function fireCanón(newShot) {
   time = 0;
   shot.xInitial = newShot.xInitial || shot.xInitial;
@@ -143,8 +181,6 @@ function fireCanón(newShot) {
 }
 
 
-
-var bullet;
 //my fire function
 
 function p1fire() {
@@ -155,8 +191,8 @@ var vel = velocityInput.value;
 //multiple parsed input value times 3.14/180 for conversion to radians
 var theta = (angleInput.value)
 
-x = 50
-y = 400
+x = 95
+y = 365
 angle = theta;
 vel = vel/100
 var bullet = new Pow(x,y,vel,angle)
@@ -173,8 +209,8 @@ var vel = velocityInput.value;
 var adjVel = vel/100
 var theta = angleInput.value;
 var adjtheta = 180 - theta;  //mirrors the input angle across the the y axis
-x = 550
-y = 400
+x = 655
+y = 365
 // angle = theta;
 vel = vel/100
 var bullet = new Pow(x,y,adjVel,adjtheta)
@@ -185,99 +221,107 @@ launchButton2.disabled = true;
 
 
 
+function whosTurnIsIt(count) {
 
+  if(count % 2 === 0) {
+  console.log("player 1's turn")
+  launchButton1.disabled = false;
 
+  } else {
+  console.log("player 2's turn")
+  launchButton2.disabled = false;
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function fire0() {
-    var canvas = document.getElementById('myCanvas');
-    var angleInput = document.getElementById('angle');
-    var velocityInput = document.getElementById('velocity');
-    var launchButton = document.getElementById('launchButton');
-
-
-    var velocity = velocityInput.value;
-    //multiple parsed input value times 3.14/180 for conversion to radians
-    var theta = (angleInput.value) * (Math.PI/180);
-
-
-
-
-
-
-
-
-
-
-
-
-    //interval between frames, intent is to be used later as a settimeout
-    //refresh value between frame renders. experiment with number, aim for 60 fps?
-    var interval = 100;
-    var t = 0;
-
-    //particle start positions for now
-    var yInitial = 400;
-    var x_n = 50;
-    var y_n = 0;
-    //Velocity of our object along the x axis.
-    var xVelocity = (velocity * Math.cos(theta));
-
-    //draw bullet
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = 'black';
-    launchButton.disabled = true;
-
-    //render my ctx(in this case, bullet) every 'interval'
-    var refreshInterval = setInterval(function() {
-      world();
-      render(ctx);
-    }, interval);
-
-    //sooooo much of this was hardcoded just to work, i fear
-    //that going further will require abandoning it all
-    function render(ctx) {
-
-      //time incremental increase
-      t = t + 1;
-      //arbitrary constant -2.5 to keep velocity input guesses high
-      velocity = velocity - 2.5;
-
-      //Velocity of Y
-      y_n = yInitial - (velocity * t * Math.sin(theta));
-      //simulate gravity on Y
-      y_n = y_n + (t + (.3 * 9.8 * t));
-      //Constant velocity over time
-      x_n = (x_n + xVelocity);
-
-
-      //if y bullet position underground release 'holds'
-      if (y_n > yInitial) {
-        clearInterval(refreshInterval);
-        launchButton.disabled = false;
-        return;
-      }
-      // draw projectile
-      ctx.fillRect(x_n, y_n, 3, 3);
-    }
 }
 
+
+
+
+
+
+function checkPlayerHitByBullet(time) {
+//if bullet position is inside tank hitboxes,
+//add or subtract
+  if(
+      (positionAtTime(time)[0] > 664  && positionAtTime(time)[1] > 381)  &&
+      (positionAtTime(time)[0] < 697  && positionAtTime(time)[1] < 398)
+    ) {
+
+      console.log("PLAYER 2 BEEN HIT!")
+      p1Wins();
+      return true;
+      }
+  if(
+      (positionAtTime(time)[0] > 55  && positionAtTime(time)[1] > 381)  &&
+      (positionAtTime(time)[0] < 86  && positionAtTime(time)[1] < 398)
+    ) {
+
+      console.log("PLAYER 1 BEN BIT")
+      p2Wins();
+      return true;
+      }
+
+  return false;
+}
+
+
+
+function p1Wins(){
+  console.log("bullet entered p2 tank area");
+}
+
+function p2Wins(){
+  console.log("bullet entered p1 tank area");
+}
+
+//need a function to detect collision
+// function collisionDetect(default) {
+
+// }
+
+
+// function playersStillAlive(empty) {
+//  if (victory )
+
+// }
+
+// function victoryToggle(default){
+
+// }
+// function letsPlayAGame(empty) {
+//   if (playersStillAlive() === true
+// }
+
+
+
+
+drawCanvas();
+
+
+
+
+//a commented out an event listener for determining 'hitbox' pixels on my canvas
+//that the ball has to cross to declare victory
+//(had to hardcode it a bit in that sense)
+// canvas.addEventListener("mousedown", getPosition, false);
+
+// function getPosition(event)
+// {
+//   var x = event.x;
+//   var y = event.y;
+
+//   var canvas = document.getElementById("myCanvas");
+
+//   x -= canvas.offsetLeft;
+//   y -= canvas.offsetTop;
+
+//   alert("x:" + x + " y:" + y);
+// }
 
 
 
 //$(function() {
 //world();
 //})
+
+
